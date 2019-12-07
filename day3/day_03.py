976e793d0226ebdbe09945a9569804cd3a313829
Philipp Barth <philipp-barth13@web.de>
2019-12-08 00:23:27 +0100

    not very fast solution for day3

--- day3/day_03.py
+++ day3/day_03.py
@@ -21,11 +21,60 @@ def get_list_from_string(string):
 
 
 def calc_intersection(wire_1, wire_2):
-    points_wire_1 = get_points_set(wire_1)
-    points_wire_2 = get_points_set(wire_2)
-    print("points wire1: ", points_wire_1)
-    print("points wire2: ", points_wire_2)
-    return points_wire_1.intersection(points_wire_2)
+    points_steps_wire_1 = get_points_steps_set(wire_1)
+    points_steps_wire_2 = get_points_steps_set(wire_2)
+    return get_intersections(points_steps_wire_1, points_steps_wire_2)
+
+
+def get_points_steps_set(wire):
+    points_steps = set()
+    curr_point = [0, 0]
+    steps = 0
+    for i in range(len(wire)):
+        direction = wire[i][0]
+        for j in range(0, int(wire[i][1])):
+            steps += 1
+            if direction == 'R':
+                curr_point[0] += 1
+            elif direction == 'L':
+                curr_point[0] -= 1
+            elif direction == 'U':
+                curr_point[1] += 1
+            elif direction == 'D':
+                curr_point[1] -= 1
+            points_steps.add(tuple(curr_point.copy()) + (steps,))
+    return points_steps
+
+
+def get_intersections(points_steps_1, points_steps_2):
+    points_1 = set()
+    points_2 = set()
+    for val in points_steps_1:
+        points_1.add(val[:2])
+    for val2 in points_steps_2:
+        points_2.add(val2[:2])
+    return points_1.intersection(points_2)
+
+
+def get_lowest_intersection_steps(points_steps_1, points_steps_2):
+    steps = set()
+    intersections = get_intersections(points_steps_1, points_steps_2)
+    steps1 = get_intersection_steps(intersections, points_steps_1)
+    steps2 = get_intersection_steps(intersections, points_steps_2)
+    for val1 in steps1:
+        for val2 in steps2:
+            if val1[:2] == val2[:2]:
+                steps.add(val1[2] + val2[2])
+    return min(steps)
+
+
+def get_intersection_steps(intersections, points_steps):
+    steps = set()
+    for val1 in intersections:
+        for val2 in points_steps:
+            if val1 == val2[:2]:
+                steps.add(val1[:2] + val2[2:])
+    return steps
 
 
 def calc_smallest_dis_to_point(points_set):
@@ -42,32 +91,20 @@ def calc_manhatten_dis(point_1, point_2):
     return dis
 
 
-def get_points_set(wire):
-    points = set()
-    curr_point = [0, 0]
-    for i in range(len(wire)):
-        direction = wire[i][0]
-        for j in range(0, int(wire[i][1])):
-            if direction == 'R':
-                curr_point[0] += 1
-            elif direction == 'L':
-                curr_point[0] -= 1
-            elif direction == 'U':
-                curr_point[1] += 1
-            elif direction == 'D':
-                curr_point[1] -= 1
-            points.add(tuple(curr_point.copy()))
-    return points
-
-
 # logic
-wire_list_1 = get_list_from_string(wire_str_1)
-wire_list_2 = get_list_from_string(wire_str_2)
-
 test_list_1 = get_list_from_string(test_str_1)
 test_list_2 = get_list_from_string(test_str_2)
+
+wire_list_1 = get_list_from_string(wire_str_1)
+wire_list_2 = get_list_from_string(wire_str_2)
 
 intersections = calc_intersection(wire_list_1, wire_list_2)
 
 # awnser part1
 print("smallest manhatten distance: ", calc_smallest_dis_to_point(intersections))
+
+points_steps_wire_1 = get_points_steps_set(wire_list_1)
+points_steps_wire_2 = get_points_steps_set(wire_list_2)
+
+# awnser part2
+print("lowest number of steps: ", get_lowest_intersection_steps(points_steps_wire_1, points_steps_wire_2))
