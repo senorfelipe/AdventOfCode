cb6f98cf13b63d0f44f68cb6d1ffc7014ddbf7e2
Philipp Barth <philipp-barth13@web.de>
2020-12-18 11:06:33 +0100

    start day6

---
+++ aoc_20/day06/day06.py
@@ -0,0 +1,15 @@
+import time
+
+start = time.process_time()
+
+yes_count = 0
+with open("./6.in").readlines() as data:
+    group = 0
+    for line in data:
+        pass
+
+
+
+
+
+print("took: %f seconds" % (time.process_time() - start))
